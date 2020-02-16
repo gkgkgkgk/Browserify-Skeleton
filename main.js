68206961e9295e2b7bee289740153ff95f1543e3
Gavri Kepets <gkepets@gmail.com>
2020-02-16 10:07:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const cheerio = require('cheerio');
+
+// put ur stuff here
+
+// run npm start to create bundle.js
+// make sure to npm install every package u add (dont just run npm i, run npm install cheerio)
+// open the html file in ur browser to run the code.
